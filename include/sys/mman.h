eb9a50d6a3adb35c04b820b8404b913945f3965c
Graham Kelly <gkgoat6700@gmail.com>
2025-08-29 16:59:29 +0000

    memory management hooks

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -2,7 +2,15 @@
 #include <stddef.h>
 #include <stdint.h>
 typedef uint64_t off_t;
-extern void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+extern void *mmap(void *addr, size_t length, int prot, int flags, int fd,
+                  off_t offset);
 extern int munmap(void *addr, size_t length);
 extern void *sbrk(intptr_t increment);
 #define MAP_ANONYMOUS 0x1
+
+typedef struct CkMMANHook {
+    void * userdata;
+    char(*free)(void*,void*);
+    void*(*alloc)(void*,void*,size_t,int,int,int,off_t);
+} CkMMANHook;
+extern CkMMANHook * ck_mmna_hook;
--- lib/mman.c
+++ lib/mman.c
@@ -1,15 +1,22 @@
 #include "sys/mman.h"
-#include "sys/pagesize.h"
 #include "alloc.h"
 #include "errno.h"
+#include "sys/pagesize.h"
 #include <stdint.h>
+#include <sys/mman.h>
+CkMMANHook *ck_mman_hook;
 void *mmap(void *addr, size_t length, int prot, int flags, int fd,
            off_t offset) {
   if (flags & MAP_ANONYMOUS) {
     return malloc(length);
   }
+  if (ck_mman_hook)
+    return ck_mman_hook->alloc(ck_mman_hook->userdata, addr, length, prot,
+                               flags, fd, offset);
 }
 int munmap(void *addr, size_t length) {
+  if (ck_mman_hook && ck_mman_hook->free(ck_mman_hook->userdata,addr))
+    return 0;
   free(addr);
   return 0;
 }
