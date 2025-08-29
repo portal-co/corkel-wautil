8c6ce047b9dfd90aef89f77adda28cfd79349c7b
Graham Kelly <gkgoat6700@gmail.com>
2025-08-29 16:52:40 +0000

    sbrk

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -4,4 +4,5 @@
 typedef uint64_t off_t;
 extern void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 extern int munmap(void *addr, size_t length);
+extern void *sbrk(intptr_t increment);
 #define MAP_ANONYMOUS 0x1
--- lib/mman.c
+++ lib/mman.c
@@ -1,11 +1,42 @@
-#include "alloc.h"
 #include "sys/mman.h"
-void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset){
-    if(flags & MAP_ANONYMOUS){
-        return malloc(length);
-    }
+#include "sys/pagesize.h"
+#include "alloc.h"
+#include "errno.h"
+#include <stdint.h>
+void *mmap(void *addr, size_t length, int prot, int flags, int fd,
+           off_t offset) {
+  if (flags & MAP_ANONYMOUS) {
+    return malloc(length);
+  }
 }
-int munmap(void *addr, size_t length){
-    free(addr);
-    return 0;
+int munmap(void *addr, size_t length) {
+  free(addr);
+  return 0;
+}
+void *sbrk(intptr_t increment) {
+  // sbrk(0) returns the current memory size.
+  if (increment == 0) {
+    // The wasm spec doesn't guarantee that memory.grow of 0 always succeeds.
+    return (void *)(__builtin_wasm_memory_size(0) * PAGESIZE);
+  }
+
+  // We only support page-size increments.
+  if (increment % PAGESIZE != 0) {
+    // abort();
+  }
+
+  // WebAssembly doesn't support shrinking linear memory.
+  if (increment < 0) {
+    // abort();
+  }
+
+  uintptr_t old =
+      __builtin_wasm_memory_grow(0, (uintptr_t)increment / PAGESIZE);
+
+  if (old == SIZE_MAX) {
+    errno = ENOMEM;
+    return (void *)-1;
+  }
+
+  return (void *)(old * PAGESIZE);
 }
