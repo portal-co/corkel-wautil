762c7544527399b36e636a2629f9e4820d60c42c
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-05-17 13:22:00 -0400

    [AI] build to ninja

--- source/dlmalloc/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-R=$(dirname $0)/../..
-O=${O:-$R/build/$ARCH}
-$R/compile.sh -isystem $R/source/include -c -o $O/dlmalloc.o $R/source/dlmalloc/src/dlmalloc.c
-            
